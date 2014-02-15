dd092de16bad834534f2af389aafe5ca73b6e384
Ben Scott <ben.scott@readify.net>
2014-02-15 15:48:58 +1000

    basic console app

---
+++ src/Clicky.Cli/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GitClicky.Core.Extensions;
+
+namespace Clicky.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input: {0}".FormatWith(args[0]));
+        }
+    }
+}
---
+++ src/GitClicky.Core/Extensions/StringExtensions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace GitClicky.Core.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string FormatWith(this string format, params object[] args)
+        {
+            args = args ?? new object[0];
+
+            var distinctNumberedTemplateMatches =
+                (from object match in new Regex(@"\{\d{1,2}\}").Matches(format) select match.ToString())
+                .Distinct().Count();
+            if (distinctNumberedTemplateMatches != args.Length)
+            {
+                var argsDic = GetDictionaryFromAnonObject(args[0]);
+
+                if (argsDic.Count < 1)
+                {
+                    throw new InvalidOperationException("Please supply enough args for the numbered templates or use an anonymous object to identify the templates by name.");
+                }
+
+                return argsDic.Aggregate(format, (current, o) => current.Replace("{{0}}".FormatWith(o.Key), o.Value.ToString()));
+            }
+
+            var validationInput = format;
+            for (var i = 0; i < args.Length; i++)
+            {
+                format = format.Replace("{" + i + "}", args[i] == null ? string.Empty : args[i].ToString());
+            }
+            if (validationInput == format)
+            {
+                throw new InvalidOperationException(
+                    "You can not mix template types. Use numbered templates or named ones with an anonymous object.");
+            }
+
+            return format;
+        }
+
+        private static IDictionary<string, object> GetDictionaryFromAnonObject(object args)
+        {
+            if (args == null)
+            {
+                return new Dictionary<string, object>();
+            }
+
+            return TypeDescriptor.GetProperties(args).Cast<PropertyDescriptor>()
+                .ToDictionary(
+                    property => property.Name, 
+                    property => property.GetValue(args));
+        }
+    }
+}
